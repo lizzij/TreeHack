7e970261d2172f4b7b825f2b008894e67ed4e080
eliza <elizazijinhuang@gmail.com>
2020-02-15 06:18:26 -0800

    record page working

--- frontend/flaskr/record.py
+++ frontend/flaskr/record.py
@@ -14,11 +14,5 @@ bp = Blueprint("record", __name__)
 @bp.route("/")
 def index():
     """Show all the posts, most recent first."""
-    # db = get_db()
-    # posts = db.execute(
-    #     "SELECT p.id, title, body, created, author_id, username"
-    #     " FROM post p JOIN user u ON p.author_id = u.id"
-    #     " ORDER BY created DESC"
-    # ).fetchall()
-    # return render_template("record/index.html", posts=posts)
-    return "Hello world"
+
+    return render_template("record/index.html")
